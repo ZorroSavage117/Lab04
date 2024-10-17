9feb37620dcdd19b9b14bd0def0e4ae4fa995802
Zorro <zorrosavage117@byui.edu>
2024-10-16 18:46:47 -0600

    45.5% pawn complete

--- piecePawn.h
+++ piecePawn.h
@@ -20,13 +20,14 @@ class Pawn : public Piece
 {
 public:
 	Pawn() { position = 0xff; fWhite = true; nMoves = 0; lastMove = 0; }
-	Pawn(const Position& pos, bool isWhite) : Piece(9, 9) { this->position = pos; if (isWhite) { this->fWhite = true; } else { fWhite = false; } }
-	Pawn(int c, int r, bool isWhite) : Piece(9, 9) { this->position.set(c, r); /*Col(c); this->position.setRow(r);*/ if (isWhite) { this->fWhite = true; } else { fWhite = false; } }
+	Pawn(const Position& pos, bool isWhite) : Piece(9, 9) { this->position = pos; fWhite = isWhite; nMoves = 0; lastMove = 0; }
+	Pawn(int c, int r, bool isWhite) : Piece(9, 9) { this->position.set(c, r); fWhite = isWhite; nMoves = 0; lastMove = 0; }
 	Pawn(const Piece& piece) { this->position = piece.getPosition(); this->fWhite = piece.isWhite(); this->nMoves = piece.getNMoves(); }
 	~Pawn() { }
 	const Piece& operator = (const Piece& rhs) { this->position = rhs.getPosition(); this->fWhite = rhs.isWhite(); this->nMoves = rhs.getNMoves(); return *this; }
 	bool isWhite() { return fWhite; }
 	int getNmoves() { return nMoves; }
+	void setNMoves(int n) { nMoves = n; }
 	int getLastMove() { return lastMove; }
 	PieceType getType()            const { return PAWN; }
 	void display(ogstream* pgout) const { pgout->drawPawn(position, fWhite); }
@@ -35,5 +36,5 @@ public:
 	bool isMoved() { if (nMoves > 0) return true; else return false; }
 	bool justMoved(int currentMove) { if (currentMove - 1 == lastMove) return true; else return false; }
 	void setLastMove(int currentMove) { lastMove = currentMove; nMoves += 1; }
-	void getMoves(set <Move>& moves, const Board& board) const {}
+	void getMoves(set <Move>& moves, const Board& board) const;
 };
